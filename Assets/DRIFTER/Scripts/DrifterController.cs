45fe062504e05576ce3aaf611fa2e6db6b854879
Cole O'Brien <obriencole11@gmail.com>
2018-05-03 09:22:10 -0400

    Added force-based movement.

--- Assets/DRIFTER/Scripts/DrifterController.cs
+++ Assets/DRIFTER/Scripts/DrifterController.cs
@@ -26,10 +26,20 @@ public class DrifterController : MonoBehaviour {
 	void Update () {
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        movement = (xInput * transform.right + yInput * transform.forward).normalized;
-	}
+		
+	// f = a
+	// f = v2 - v1
+	Vector3 target_velocity = new Vector3(xInput, 0.0, yInput).normalized * speed;
+	Vector3 current_velocity = _rigidbody.velocity;
+	movement = target_velocity - current_velocity;
+		
+        // movement = (xInput * transform.right + yInput * transform.forward).normalized;
+	}	
 
     private void FixedUpdate(){
-        _rigidbody.velocity = movement * speed * Time.deltaTime;
+        // _rigidbody.velocity = movement * speed * Time.deltaTime;
+    	// Switched to add force, should handle collisions better, but may 'feel' different?
+    	// I also may have got the forcemode wrong
+    	_rigidbody.addForce(movement, ForceMode.Impulse);
     }
 }
