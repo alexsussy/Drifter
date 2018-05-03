c99d8fa3b0c682fe9fac23ad6fe88defa4388680
Alex <asussman95@gmail.com>
2018-05-03 01:32:03 -0400

    Initial commit

---
+++ Assets/DRIFTER/Scripts/DrifterCamera.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrifterCamera : MonoBehaviour {
+
+    public float FOVKick;
+
+    public float smooth;
+
+    public float minX = -60f,
+        maxX = 60f;
+
+    public float minY = -360f,
+        maxY = 360f;
+
+    public float sensitivityX = 5f,
+        sensitivityY = 5f;
+
+    private GameObject CameraPivot;
+    public float rotX, rotY = 0f;
+
+
+    void Awake() {
+        CameraPivot = transform.GetChild(0).gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update () {
+        if (Input.GetButton("Fire1")){
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 30f, 0.08f);
+            sensitivityX = 3;
+            sensitivityY = 3;
+        }
+        else{
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60f, 0.08f);
+            sensitivityX = 5f;
+            sensitivityY = 5f;
+        }
+
+        rotY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotX += Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotY = Mathf.Clamp(rotY, minY, maxY);
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+	}
+
+    private void FixedUpdate(){
+        transform.localEulerAngles = new Vector3(0, rotX, 0);
+        CameraPivot.transform.localEulerAngles = new Vector3(-rotY, 0, 0);
+    }
+}
---
+++ Assets/DRIFTER/Scripts/DrifterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrifterController : MonoBehaviour {
+
+    [SerializeField]
+    private bool canJump = true;
+
+    public float speed;
+
+    private Rigidbody _rigidbody;
+    private CapsuleCollider _collider;
+
+    Vector3 movement;
+
+    private void Awake(){
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        movement = (xInput * transform.right + yInput * transform.forward).normalized;
+	}
+
+    private void FixedUpdate(){
+        _rigidbody.velocity = movement * speed * Time.deltaTime;
+    }
+}
