be0afeefffc5430064984e2c5ba16458dbcb987e
Alex <asussman95@gmail.com>
2018-05-05 15:12:18 -0400

    typo and direction fixes

--- Assets/DRIFTER/Scripts/DrifterController.cs
+++ Assets/DRIFTER/Scripts/DrifterController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class DrifterController : MonoBehaviour {
 
     [SerializeField]
-    private bool canJump = true;
+    //private bool canJump = true;
 
     public float speed;
 
     private Rigidbody _rigidbody;
-    private CapsuleCollider _collider;
+    //private CapsuleCollider _collider;
 
     Vector3 movement;
 
     private void Awake(){
         _rigidbody = GetComponent<Rigidbody>();
-        _collider = GetComponent<CapsuleCollider>();
+        //_collider = GetComponent<CapsuleCollider>();
     }
 
     void Start () {
@@ -26,20 +26,21 @@ public class DrifterController : MonoBehaviour {
 	void Update () {
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-		
-	// f = a
-	// f = v2 - v1
-	Vector3 target_velocity = new Vector3(xInput, 0.0, yInput).normalized * speed;
-	Vector3 current_velocity = _rigidbody.velocity;
+
+        // f = a
+        // f = v2 - v1
+        Vector3 target_velocity = (xInput * transform.right + yInput * transform.forward).normalized * speed;
+        Vector3 current_velocity = _rigidbody.velocity;
 	movement = target_velocity - current_velocity;
 		
-        // movement = (xInput * transform.right + yInput * transform.forward).normalized;
+    // movement = (xInput * transform.right + yInput * transform.forward).normalized;
+
 	}	
 
     private void FixedUpdate(){
         // _rigidbody.velocity = movement * speed * Time.deltaTime;
     	// Switched to add force, should handle collisions better, but may 'feel' different?
     	// I also may have got the forcemode wrong
-    	_rigidbody.addForce(movement, ForceMode.Impulse);
+    	_rigidbody.AddForce(movement, ForceMode.VelocityChange);
     }
 }
